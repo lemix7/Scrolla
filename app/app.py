a9c193c48b5e22b5eaf2352ead99312e42ff6b93
Ahmad <ahmedgeger@gmail.com>
2026-02-04 03:44:40 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI , HTTPException
+from app.schemas import Post
+
+
+fast_app = FastAPI()
+
+text_posts = {1: {'title': '48hours',
+                  'caption': 'How to change your life in 48 hours'},
+              2: {'title': 'Nicosia',
+                  'caption': 'The only shared capital'}}
+
+
+@fast_app.get('/posts')
+def get_post():
+    return text_posts
+
+
+@fast_app.post('/create')
+def create_post(post: Post):
+    new_id = max(text_posts.keys()) + 1
+    text_posts[new_id] = {
+        'title': post.title,
+        'caption': post.caption
+    }
+
+    return {'id': new_id, 'post': text_posts[new_id]}
+
+
+@fast_app.get('/post{id}')
+def get_post_by_id(id: int):
+    return text_posts.get(id)
+
+@fast_app.delete('/post')
+def delete_post(id:int):
+    del text_posts[id]
+    return {'post deleted successfully'}
+
+
